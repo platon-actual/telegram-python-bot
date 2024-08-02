2b08aeada3adc25aff348d80c39d2fcf2432955a
Ramiro Ivan Rios <ramiro.ivan.rios@gmail.com>
2024-08-02 02:57:54 -0300

    Probando requests para subir al backend en flask

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from bot_token import TOKEN
 
 from urllib.request import urlretrieve
 import telebot
+import requests
 
 PHOTO_DL_URL = "https://api.telegram.org/file/bot" + TOKEN + "/"
 
@@ -25,15 +26,22 @@ def send_same_photo(message):
     #for photo_x in message.photo:
     #    print (photo_x.file_id)
     #print (bot.get_file(message.photo[0].file_id))
-    photo1 = bot.get_file(message.photo[0].file_id)
-    photo2 = bot.get_file(message.photo[1].file_id)
+    #photo1 = bot.get_file(message.photo[0].file_id)
+    #photo2 = bot.get_file(message.photo[1].file_id)
     photo3 = bot.get_file(message.photo[2].file_id)
-    print (photo1)
-    print (photo2)
+    #print (photo1)
+    #print (photo2)
     print (photo3)
-    urlretrieve(PHOTO_DL_URL + photo2.file_path, "download/" + photo2.file_path)
+    urlretrieve(PHOTO_DL_URL + photo3.file_path, "download/" + photo3.file_path)
 
-    bot.reply_to(message, "Oh, una foto! Aún no sé usarla...")
+    bot.reply_to(message, "Oh, una foto! la descargaré")
+    bot.send_message(message.chat.id, "puede descargarla de https://ramirorios.pythonanywhere.com/")
     bot.send_photo(message.chat.id, message.photo[1].file_id)
+
+    url = '192.168.1.131:4000/post_image'
+    my_photo = {'image': open("download/" + photo3.file_path, 'rb')}
+    x = requests.post(url, files=my_photo)
+    print(x.text)
+
 
 bot.infinity_polling()
